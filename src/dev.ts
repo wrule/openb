dbcd8d36e71908dd2daf514fd0e289f4bdc8d535
wrule <1982775886@qq.com>
2024-04-18 21:16:50 +0800

    还需要继续思考逻辑

--- src/dev.ts
+++ src/dev.ts
@@ -1,7 +1,33 @@
 import { hello } from '.';
 
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+
+function getConfig(filePath: string) {
+  try {
+    return require(filePath);
+  } catch (error) {
+    console.error(`cannot load ${filePath}`);
+  }
+  return { };
+}
+
+function openBrowser(
+  userDataDir: string,
+  browserPath = '',
+  args = '--flag-switches-begin --flag-switches-end',
+) {
+  const config = getConfig(path.join(os.homedir(), '.cbmgr/config.json'));
+  return exec(`"${browserPath || config.browserPath}" ${args} --user-data-dir="${userDataDir}"`);
+}
+
 function dev() {
-  hello();
+  getConfig();
+  // console.log(path.join(os.homedir(), '.cbmgr/'));
+  // const chrome = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  // openBrowser(chrome, '~/Desktop/1');
 }
 
 dev();
