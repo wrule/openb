9fd49afe7ee6403693c983dbe6c40e1197e8b478
wrule <1982775886@qq.com>
2024-04-21 15:19:00 +0800

    转移index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,64 @@
+import os from 'os';
+import path from 'path';
+import { exec } from 'child_process';
 
-export
-function hello() {
-  console.log('hello world!');
+interface Config {
+  browserPath?: string;
+  args?: string;
+  userDataDir?: string;
 }
+
+function loadConfig(configPath: string): Config {
+  try {
+    return require(configPath);
+  } catch (error) {
+    console.warn(`it is recommended to configure ${configPath}`);
+  }
+  return { };
+}
+
+const globalConfig = loadConfig(path.resolve(os.homedir(), '.cbmgr/config.json'));
+
+function getUserDataDir(text: string) {
+  if (text === path.basename(text) && globalConfig.userDataDir)
+    return path.resolve(globalConfig.userDataDir, text);
+  return path.resolve(text);
+}
+
+function openBrowser(
+  userDataDir: string,
+  browserPath?: string,
+  args?: string,
+) {
+  const realUserDataDir = getUserDataDir(userDataDir);
+  const userConfig = loadConfig(path.resolve(realUserDataDir, 'config.json'));
+  const params: Config = {
+    ...globalConfig,
+    ...userConfig,
+    ...JSON.parse(JSON.stringify({ browserPath, args })),
+  };
+  params.userDataDir = realUserDataDir;
+  if (!params.args) params.args = '--flag-switches-begin --flag-switches-end';
+  console.log(params);
+  if (!params.browserPath) {
+    console.error('browserPath is empty');
+    process.exit(1);
+  }
+  if (!params.userDataDir) {
+    console.error('userDataDir is empty');
+    process.exit(1);
+  }
+  return exec(`"${
+    params.browserPath
+  }" ${
+    params.args ?? ''
+  } --user-data-dir="${
+    params.userDataDir
+  }"`);
+}
+
+function main() {
+  openBrowser('u1');
+}
+
+main();
